680038f16f5d4e39d8e9b9fe783c08103d5eecbf
Igor Tsialiuk <igor.tsialiuk@burberry.com>
2020-11-04 17:28:11 +0300

    Task 4 - Update post /products url

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -108,9 +108,14 @@ export default function PageProductForm() {
 
   const onSubmit = (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
-    const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
-    axios.put(`${API_PATHS.bff}/product`, productToSave)
-      .then(() => history.push('/admin/products'));
+
+    if (id) {
+      axios.put(`${API_PATHS.bff}/products`, { ...ProductSchema.cast(formattedValues), id })
+        .then(() => history.push('/admin/products'));
+    } else {
+      axios.post(`${API_PATHS.bff}/products`, formattedValues)
+        .then(() => history.push('/admin/products'));
+    }
   };
 
   useEffect(() => {
@@ -118,7 +123,7 @@ export default function PageProductForm() {
       setIsLoading(false);
       return;
     }
-    axios.get(`${API_PATHS.bff}/product/${id}`)
+    axios.get(`${API_PATHS.bff}/products/${id}`)
       .then(res => {
         setProduct(res.data);
         setIsLoading(false);
