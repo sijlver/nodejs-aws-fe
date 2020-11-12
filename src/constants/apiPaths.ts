bc766ad608ba0435f8659d0a8839ea17d562d966
Igor Tsialiuk <igor.tsialiuk@burberry.com>
2020-11-12 09:37:25 +0300

    Update import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vo4gbkx7j0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://a7uftfm800.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
