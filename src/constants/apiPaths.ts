4b51b71379865c836e8609ce9e8f320f1faf9e9b
Igor Tsialiuk <igor.tsialiuk@burberry.com>
2020-12-02 19:43:35 +0300

    Add cart endpoint for eb task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://vo4gbkx7j0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://a7uftfm800.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  cart: 'http://cart-api-service-dev.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
